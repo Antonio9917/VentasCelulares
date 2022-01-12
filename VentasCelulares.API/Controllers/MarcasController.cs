c0e6660e33ff5c76f99bb354f58fa0235a99eb56
antonio9907 <tono007111.jazl@gmail.com>
2022-01-12 10:38:37 -0700

    Agregar archivos de proyecto.

---
+++ VentasCelulares.API/Controllers/MarcasController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VentasCelulares.API.Modelos;
+
+namespace VentasCelulares.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarcasController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var marca = new List< Marcas>();
+
+            marca.Add(new Marcas { idMarca = 1, Nombre = "Xiaomi", GrupoMarca = "Motorola" });
+            marca.Add(new Marcas { idMarca = 2, Nombre = "Samsung", GrupoMarca = "Huawei" });
+
+
+
+            return Ok(marca);
+        }
+
+        [HttpGet]
+        [Route("{nombre}")]
+        public IActionResult GetNombre(string nombre)
+        {
+            return Ok("El nomnbre de la marca es:" + nombre);
+        }
+    }
+}
---
+++ VentasCelulares.API/Modelos/Marcas.cs
@@ -0,0 +1,9 @@
+﻿namespace VentasCelulares.API.Modelos
+{
+    public class Marcas
+    {
+        public int idMarca { get; set; }
+        public string Nombre { get; set; }
+        public string GrupoMarca { get; set; }
+    }
+}
